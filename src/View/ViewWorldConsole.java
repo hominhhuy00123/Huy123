d629767ce44e2d5e393a7a91433d82a346ebda14
Huy <huyhmde130141@fpt.edu.vn>
2019-01-25 15:57:04 +0700

    4nd

--- src/View/ViewWorldConsole.java
+++ src/View/ViewWorldConsole.java
@@ -7,8 +7,17 @@ public class ViewWorldConsole {
 		world world1 = new world(5,7);
 			for (int i=0; i<world1.nRow; i++ )
 				for (int j=0; j<world1.nColumn; j++ ) {
-					System.out.print(String.format("%2f", world1.getCells(i, j)));
+					System.out.print(String.format("%2f ", world1.getCells(i,j)));
 	}
 			System.out.println();
+	
+	try {
+		Thread.sleep(world.TIME_STEP);
+		}
+	
+			catch(Exception e) {
+				e.printStackTrace();		}
 	}
 }
+
+
--- src/control/world.java
+++ src/control/world.java
@@ -2,10 +2,12 @@ package control;
 
 import java.util.Random;
 
-public class world {
+public class world extends Thread {
 	public int nRow;
 	public int nColumn;
 	private double[][] Cells; 
+	private double cellsGrowSpeed=0.1;
+	public static int TIME_STEP=1000;
 	public world(int x, int y ) {
 		nRow = x;
 		nColumn = y;
@@ -20,5 +22,28 @@ public class world {
 	public double getCells(int i, int j) {
 		return Cells[i][j];
 	}
+	public void run() {
+		Random ran = new Random();
+		while(true) {
+			for(int i=0; i<nRow; i++) {
+				for (int j=0; j<nColumn; j++) {
+					double x = ran.nextDouble();
+					Cells[i][j] = Cells[i][j]+ x * cellsGrowSpeed;
+					if (Cells[i][j]>1) {
+						Cells[i][j] = 1 ;
+					}
+				}
+			}
+			
+		try {
+			Thread.sleep(world.TIME_STEP);
+			}
+		
+				catch(Exception e) {
+					e.printStackTrace();		}
+		}
 	}
+		
+	}
+	
 

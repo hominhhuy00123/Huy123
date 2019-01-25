2e6d4f93352bfd61190b2be0430400f4c15f4382
Huy <huyhmde130141@fpt.edu.vn>
2019-01-25 15:14:11 +0700

    code1

--- src/View/ViewWorldConsole.java
+++ src/View/ViewWorldConsole.java
@@ -2,12 +2,13 @@ package View;
 
 import control.world;
 
-public class ViewWorld {
+public class ViewWorldConsole {
 	public static void main(String[] args) {
 		world world1 = new world(5,7);
 			for (int i=0; i<world1.nRow; i++ )
 				for (int j=0; j<world1.nColumn; j++ ) {
-					System.out.println(String.format("%2f", world1.getCells(i, j)));
+					System.out.print(String.format("%2f", world1.getCells(i, j)));
 	}
+			System.out.println();
 	}
 }

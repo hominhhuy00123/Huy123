167055f73970e798aecb0538d5023907db84e283
Huy <huyhmde130141@fpt.edu.vn>
2019-01-25 16:16:59 +0700

    5nd

---
+++ src/control/Animal.java
@@ -0,0 +1,32 @@
+package control;
+
+public class Animal extends Thread implements IAnimal {
+	protected double energy;
+	protected boolean iMale;
+	protected int speed;
+	protected world world;
+	@Override
+	public Object eat(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object move(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object die(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object mate(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/control/IAnimal.java
@@ -0,0 +1,9 @@
+package control;
+
+public interface IAnimal {
+	abstract public Object eat(Object o);
+	abstract public Object move(Object o);
+	abstract public Object die(Object o);
+	abstract public Object mate(Object o);
+	
+}
